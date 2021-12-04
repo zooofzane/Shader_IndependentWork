9407fd10989ec3fd101dd6045ec5e0cf39e351b9
zooofzane <yu_an12@163.com>
2021-12-04 18:02:14 -0500

    plane

--- dist/terrain.e77e9324.js
+++ dist/terrain.e77e9324.js
@@ -36426,7 +36426,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "50061" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "54988" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
